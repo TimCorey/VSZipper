fcfdad78664fc2444ec53a5e4f1dae35d3fdb92b
calebtt <mounteriemachine@hotmail.com>
2022-04-04 14:21:29 -0500

    Added error logging method, useful for re-routing error logging if desired. Modified LINQ Where clause to use Count instead.

--- ZipperLibrary/Zipper.cs
+++ ZipperLibrary/Zipper.cs
@@ -1,5 +1,5 @@
 ﻿using System.IO.Compression;
-
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 
 namespace ZipperLibrary;
@@ -13,7 +13,7 @@ public class Zipper
         IConfigurationSection? exclusionSection = config.GetSection("Exclusions");
         string[]? excludedPathsAndExtensions = exclusionSection.Get<string[]>();
 
-        foreach ( string excludedPathOrExtension in excludedPathsAndExtensions )
+        foreach (string excludedPathOrExtension in excludedPathsAndExtensions)
         {
             exclusions.Add(excludedPathOrExtension);
         }
@@ -37,17 +37,32 @@ public class Zipper
                 .GetFiles("*", SearchOption.AllDirectories)
                 .Where(x => (x.Attributes & FileAttributes.Hidden) == 0 && x.FullName != zipFile);
 
-            foreach ( var f in files )
+            foreach (var f in files)
             {
-                if ( exclusions.Where(x => f.FullName.Contains(x)).Count() == 0 )
+                if (exclusions.Count(x => f.FullName.Contains(x)) == 0)
                 {
                     archive.CreateEntryFromFile(f.FullName, f.FullName.Substring(folder.Length));
                 }
             }
         }
-        catch ( Exception ex )
+        catch (Exception ex)
         {
-            Console.WriteLine(ex.Message);
+            LogError(ex.Message, this);
         }
+    }
+
+    /// <summary> A singular function for error messages, can be re-routed from here, logged to a file, etc. </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="originator">The handle to the object making the error logging call.</param>
+    private static void LogError(string message, Object? originator = null)
+    {
+        string callerName = "";
+        if (originator != null)
+        {
+            var t = originator.GetType();
+            if (t.FullName != null)
+                callerName = "Error in: " + t.FullName + Environment.NewLine;
+        }
+        Console.WriteLine(callerName + message);
     }
 }
